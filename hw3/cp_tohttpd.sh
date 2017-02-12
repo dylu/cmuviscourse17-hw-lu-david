95e4457c4f3db376d1a58745181e2c59574b447d
David L <davidylu@cmu.edu>
2017-02-12 15:00:17 -0500

    hw3: initializing hw3 folder with starter code.

---
+++ hw3/cp_tohttpd.sh
@@ -0,0 +1,6 @@
+
+rm -r /mnt/c/dlu/programs/portable/zwamp-x64-2.2.1/vdrive/web
+mkdir /mnt/c/dlu/programs/portable/zwamp-x64-2.2.1/vdrive/web
+
+cp -r . /mnt/c/dlu/programs/portable/zwamp-x64-2.2.1/vdrive/web/
+rm /mnt/c/dlu/programs/portable/zwamp-x64-2.2.1/vdrive/web/cp_tohttpd.sh
